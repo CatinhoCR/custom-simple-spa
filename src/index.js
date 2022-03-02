a7d7b9d4a8713d92fa84daf2e6da3340fc6395a2
Cato <cato.castques@gmail.com>
2022-03-02 00:22:31 -0600

    simpler scss starting point

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('Interesting!')
+// Include css for webpack (for development only)
+// const css = require('../scss/main.scss') // eslint-disable-line no-unused-vars
+import 'styles/app.scss'
+
+// Hot reloading (for development only)
+if (module.hot) {
+  module.hot.accept()
+}
+
+class App {
+  constructor() {
+    this.msg = ''
+    this.init()
+  }
+
+  init() {
+    this.msg = 'hi bru'
+    console.log(this.msg)
+  }
+}
+
+const app = new App() // eslint-disable-line no-unused-vars
