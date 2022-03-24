3645800c13f5a1ad0fd1f7ed3012c5ca3a4dcccc
Cato <cato.castques@gmail.com>
2022-03-24 03:14:26 -0600

    Fixing vulnerabilities and others

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -43,45 +43,12 @@ module.exports = {
   entry: {
     app: path.resolve(environment.paths.src, 'index.js'),
   },
-  // entry: [environment.paths.src + '/index.js'],
 
   // Where webpack outputs the assets and bundles
   output: {
     path: environment.paths.build,
     filename: '[name].bundle.js',
-    // publicPath: '/',
   },
-
-  /*
-  // Customize the webpack build process
-  plugins: [
-    // Removes/cleans build folders and unused assets when rebuilding
-    new CleanWebpackPlugin(),
-
-    // Copies files from target to destination folder
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: environment.paths.public,
-          to: 'assets',
-          globOptions: {
-            ignore: ['*.DS_Store'],
-          },
-          noErrorOnMissing: true,
-        },
-      ],
-    }),
-
-    // Generates an HTML file from a template
-    // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
-    new HtmlWebpackPlugin({
-      title: 'webpack Boilerplate',
-      favicon:  path.resolve(environment.paths.src, 'images', 'favicon.png'),
-      template:  path.resolve(environment.paths.src, 'index.html'), // template file
-      filename: 'index.html', // output file
-    }),
-  ],
-  */
 
   module: {
     rules: [
